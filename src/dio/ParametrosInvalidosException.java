3a5a5ac30e547f6fd58193a9191682e5321f98c0
Hellyton <hellyton_cs@hotmail.com>
2023-05-18 21:28:56 -0300

    entrega desafio de projeto dio

---
+++ src/dio/ParametrosInvalidosException.java
@@ -0,0 +1,11 @@
+package dio;
+
+public class ParametrosInvalidosException extends Exception {
+
+    public static void main(String args[]) throws ParametrosInvalidosException
+    {
+        System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+
+    }
+
+}
